71df3aca32d5cbeb79169e4f887bcb79fd4e3ecd
ValueXu <valuexu@outlook.com>
2021-04-19 14:22:39 +0800

    完善主页头部的用户信息

---
+++ src/components/user_info/UserInfo.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Descriptions } from "antd";
+import { userType } from "../../utils/typeMap";
+
+export default class UserInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div
+        className="user_info"
+        style={{ display: "inline-block", width: "14rem" }}
+      >
+        <Descriptions size="small" column={2}>
+          <Descriptions.Item label="用户名" key={0}>
+            {this.props.name}
+          </Descriptions.Item>
+          <Descriptions.Item label="用户等级" key={1}>
+            {userType(this.props.type)}
+          </Descriptions.Item>
+        </Descriptions>
+      </div>
+    );
+  }
+}

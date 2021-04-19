00529dd06ec0fd23c58a1382e3a1e766e091a07c
ValueXu <valuexu@outlook.com>
2021-04-19 14:20:05 +0800

    完善主页头部和主页样式

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { PageHeader } from "antd";
+import { connect } from "react-redux";
+import UserInfo from "../user_info/UserInfo";
 
-export default class Header extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+// import { userType } from "../../utils/typeMap";
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const userInfo = this.props.userInfo;
+    const menuInfo = this.props.menuInfo;
+    return (
+      <div className="header">
+        <PageHeader
+          ghost={false}
+          onBack={() => window.history.back()}
+          title={menuInfo.title}
+          subTitle={menuInfo.content}
+          extra={<UserInfo name={userInfo.name} type={userInfo.type} />}
+        ></PageHeader>
+      </div>
+    );
+  }
 }
+
+const stateToProps = (state) => {
+  const props = {
+    menuInfo: state.menuChangeReducer.menuInfo,
+    userInfo: state.signChangeReducer.userInfo,
+  };
+  return props;
+};
+
+export default connect(stateToProps)(Header);
